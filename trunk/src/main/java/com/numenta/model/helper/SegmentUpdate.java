9bb19277add1edf902d315cf8bb32fa21fe8aa41
vincentvanrijn@hotmail.com <vincentvanrijn@hotmail.com@19daf505-75c8-7eda-6e66-a2cc32a78e90>
2011-02-13 14:06:31 +0000

    forgot to post this

---
+++ trunk/src/main/java/com/numenta/model/helper/SegmentUpdate.java
@@ -0,0 +1,40 @@
+package com.numenta.model.helper;
+
+import java.util.List;
+
+import com.numenta.model.Synapse;
+
+public class SegmentUpdate {
+
+	private boolean sequenceSegment;
+	private int segmentUpdateIndex;
+	private List<Synapse> activeSynapses;
+	public static boolean POSITIVE_REINFORCEMENT=true;
+	public static boolean NO_POSITIVE_REINFORCEMENT=false;
+	
+	public int getSegmentUpdateIndex() {
+		return segmentUpdateIndex;
+	}
+
+	public void setSegmentUpdateIndex(int segmentUpdateIndex) {
+		this.segmentUpdateIndex = segmentUpdateIndex;
+	}
+
+	public List<Synapse> getActiveSynapses() {
+		return activeSynapses;
+	}
+
+	public void setActiveSynapses(List<Synapse> activeSynapses) {
+		this.activeSynapses = activeSynapses;
+	}
+
+	public boolean isSequenceSegment() {
+		return sequenceSegment;
+	}
+
+	public void setSequenceSegment(boolean sequenceSegment) {
+		this.sequenceSegment=sequenceSegment;
+		
+	}
+
+}
